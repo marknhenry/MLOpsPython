342d3e311b1cd0942f041b3713dff697f0d1edc7
Mark Henry <marknhenry@live.com>
2021-08-04 22:52:46 +0200

    fixed dataset id issue

--- diabetes_regression/register/register_model.py
+++ diabetes_regression/register/register_model.py
@@ -125,15 +125,15 @@ def main():
         print(f"Tags present: {parent_tags}")
 
     if (model is not None):
-        dataset_id = parent_tags["dataset_id"]
+        # dataset_id = parent_tags["dataset_id"]
         if (build_id is None):
             register_aml_model(
                 model_file,
                 model_name,
                 model_tags,
                 exp,
-                run_id,
-                dataset_id)
+                run_id)
+                # dataset_id)
         elif (build_uri is None):
             register_aml_model(
                 model_file,
@@ -141,7 +141,7 @@ def main():
                 model_tags,
                 exp,
                 run_id,
-                dataset_id,
+                # dataset_id,
                 build_id)
         else:
             register_aml_model(
@@ -150,7 +150,7 @@ def main():
                 model_tags,
                 exp,
                 run_id,
-                dataset_id,
+                # dataset_id,
                 build_id,
                 build_uri)
     else:
@@ -175,7 +175,7 @@ def register_aml_model(
     model_tags,
     exp,
     run_id,
-    dataset_id,
+    # dataset_id,
     build_id: str = 'none',
     build_uri=None
 ):
@@ -196,7 +196,8 @@ def register_aml_model(
             model_path=model_path,
             tags=tagsValue,
             datasets=[('training data',
-                       Dataset.get_by_id(exp.workspace, dataset_id))])
+            # 'diabetes_ds in the line below needs to be parameterized
+                       Dataset.get_by_name(exp.workspace, 'diabetes_ds'))])
         os.chdir("..")
         print(
             "Model registered: {} \nModel Description: {} "
