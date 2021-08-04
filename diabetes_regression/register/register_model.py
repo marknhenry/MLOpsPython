c8ccad89084e734dbbf55727f2ae817837ee1ce7
Mark Henry <marknhenry@live.com>
2021-08-05 01:40:44 +0400

    Feature/deploy aks (#2)
    * Added CD Pipeline for AKS
    
    * Added CD Pipeline for AKS
    
    * Added trigger on feature branch
    
    * fixed dataset id issue
    
    * fixed linting issue
    
    * fixed linting issue 2
    
    * fixed linting issue 3
    
    * fixed linting issue 4
    
    * Adding Conda Requirements File

--- diabetes_regression/register/register_model.py
+++ diabetes_regression/register/register_model.py
@@ -125,15 +125,14 @@ def main():
         print(f"Tags present: {parent_tags}")
 
     if (model is not None):
-        dataset_id = parent_tags["dataset_id"]
+        # dataset_id = parent_tags["dataset_id"]
         if (build_id is None):
             register_aml_model(
                 model_file,
                 model_name,
                 model_tags,
                 exp,
-                run_id,
-                dataset_id)
+                run_id)
         elif (build_uri is None):
             register_aml_model(
                 model_file,
@@ -141,7 +140,7 @@ def main():
                 model_tags,
                 exp,
                 run_id,
-                dataset_id,
+                # dataset_id,
                 build_id)
         else:
             register_aml_model(
@@ -150,7 +149,7 @@ def main():
                 model_tags,
                 exp,
                 run_id,
-                dataset_id,
+                # dataset_id,
                 build_id,
                 build_uri)
     else:
@@ -175,7 +174,7 @@ def register_aml_model(
     model_tags,
     exp,
     run_id,
-    dataset_id,
+    # dataset_id,
     build_id: str = 'none',
     build_uri=None
 ):
@@ -195,8 +194,9 @@ def register_aml_model(
             model_name=model_name,
             model_path=model_path,
             tags=tagsValue,
+            # 'diabetes_ds in the line below needs to be parameterized
             datasets=[('training data',
-                       Dataset.get_by_id(exp.workspace, dataset_id))])
+                       Dataset.get_by_name(exp.workspace, 'diabetes_ds'))])
         os.chdir("..")
         print(
             "Model registered: {} \nModel Description: {} "
--- mark-readme.txt
+++ mark-readme.txt
@@ -5,3 +5,12 @@ Provision Resources
 Add AML Service Connection
 Run BUild CI Pipeline
 Run Build CD Pipeline
+
+Setting Up Python Environment: 
+------------------------------
+conda create -y -n py3.6.2-azureml python=3.6.2
+conda activate py3.6.2-azureml
+conda update -y -n base -c defaults conda
+conda install -y ipykernel nb_conda
+ipython kernel install --user --name py3.6.2-azureml --display-name "Python (py3.6.2-azureml)"
+pip install azureml-core tqdm pandas_ml azureml-datadrift azureml-widgets azureml-sdk azureml-dataset-runtime azureml-pipeline azureml-pipeline-steps azureml-opendatasets python-dotenv autopep8
